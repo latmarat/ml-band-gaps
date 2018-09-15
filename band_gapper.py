9d831f8a2b8919b87a35fc9d898baff75e5db4e8
Marat Latypov <latmarat@gmail.com>
2018-09-15 12:14:51 -0700

    New fitting functions

--- band_gapper.py
+++ band_gapper.py
@@ -5,134 +5,198 @@
 import pandas as pd
 import requests
 
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 
+from matminer.data_retrieval.retrieve_MP import MPDataRetrieval
+from matminer.utils.conversions import str_to_composition
+from matminer.utils.conversions import dict_to_object
+from matminer.utils.conversions import composition_to_oxidcomposition
+
+from matminer.featurizers.structure import DensityFeatures
+from matminer.featurizers.structure import RadialDistributionFunction
+from matminer.featurizers.composition import OxidationStates
+from matminer.featurizers.composition import ValenceOrbital
+
+from pymatgen.core.composition import Composition
+from pymatgen import Structure
 from pymatgen.io.cif import CifParser
 
 def digest(url):
-  '''Get structure for given url of CIF file'''
+	'''Get structure for given url of CIF file'''
 
-  response = requests.get(url)
-  data = response.text
+	response = requests.get(url)
+	data = response.text
 
-  parser = CifParser.from_string(data)
-  structure = parser.get_structures()[0]
-  print('Successfuly read structure for',structure.composition)
-  
-  return structure
+	parser = CifParser.from_string(data)
+	structure = parser.get_structures()[0]
+	print('Successfuly read structure for',structure.composition)
 
-def train(x,y,nestim=50,split=True,test_frac=0.1):
-    '''Train RF model'''
-
-    ranfor = RandomForestRegressor(n_estimators=nestim,
-                            oob_score=True, random_state=42, verbose=0)
-
-    # split train and test data
-    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=test_frac, random_state=42)
-    print('Split data with %.3f test fraction' % test_frac)
-
-    # fit RF
-    ranfor.fit(x_train,y_train)
-    print('Fit RF model using %d estimators ' % nestim)
-
-    # use RF to predict
-    y_hat_train = ranfor.predict(x_train) 
-    y_hat_test = ranfor.predict(x_test) 
-
-    # print errors
-    mae_train = np.mean(abs(y_hat_train-y_train))/np.mean(y_train)
-    print('Train error: %.3f ' % mae_train)
-
-    mae_test = np.mean(abs(y_hat_test-y_test))/np.mean(y_test)
-    print('Test error : %.3f ' % mae_test)
-
-    return ranfor
+	return structure
 
 def query_data(pname,api_key,path=''):
 
+	mpdr = MPDataRetrieval(api_key)
 
-    mpdr = MPDataRetrieval(api_key)
+	# query properties
+	props = mpdr.get_dataframe(criteria={pname: {"$exists": True},
+	#                                      "elements": {"$all": ["Li", "Fe", "O"]},
+										("{}.warnings".format(pname)): None},
+							  properties=['pretty_formula',pname,'e_above_hull'])
+	print("There are {} entries satisfying criteria".format(props[pname].count()))
 
-    # query properties
-    props = mpdr.get_dataframe(criteria={pname: {"$exists": True},
-#                                      "elements": {"$all": ["Li", "Fe", "O"]},
-                                       ("{}.warnings".format(pname)): None},
-                             properties=['pretty_formula',pname,'e_above_hull'])
-    print("There are {} entries satisfying criteria".format(props[pname].count()))
+	# Load crystal structures
+	# initialize dataframe
+	structures = pd.DataFrame(columns=['structure'])
 
-    # Load crystal structures
-    # initialize dataframe
-    structures = pd.DataFrame(columns=['structure'])
+	# lists of mp ids to avo
+	chunk_size = 1000
+	mp_ids = props.index.tolist()
+	sublists = [mp_ids[i:i+chunk_size] for i in range(0, len(mp_ids), chunk_size)]
 
-    # lists of mp ids to avo
-    chunk_size = 1000
-    mp_ids = props.index.tolist()
-    sublists = [mp_ids[i:i+chunk_size] for i in range(0, len(mp_ids), chunk_size)]
+	# query structures 
+	for sublist in sublists:
+	structures = structures.append(mpdr.get_dataframe({"material_id":{"$in": sublist}}, ['structure']))
 
-    # query structures 
-    for sublist in sublists:
-        structures = structures.append(mpdr.get_dataframe({"material_id":{"$in": sublist}}, ['structure']))
-    
-    data = pd.concat([props,structures],axis=1)
-    fname = '%s/%s.pkl' % (path,pname)
+	data = pd.concat([props,structures],axis=1)
+	fname = '%s/%s.pkl' % (path,pname)
 
-    data.to_pickle(fname)
-    print('Saved file to ',fname)
-    
-    return data
+	data.to_pickle(fname)
+	print('Saved file to ',fname)
 
-def predict(x_new,model):
-    '''Predict using trained model'''
-
-    y_new = model.predict(x_new)
-
-    return y_new
-
-def use(formula,url):
-    '''Use model for new structures'''
-
-    # get structure from CIF
-    s = digest(url)
-
-    atom_feats = get_atom_feats(formula)
-    lat_feats = get_crys_feats(formula)
-    rdf = radial_dist_func(s)
-
-    # initialize dataframe
-    crys_data = pd.DataFrame(columns=['pretty_formula','initial_structure',
-                  'atomic_features','lattice_features','radial_dist','chem'])
-
-    crys_data["composition"] = str_to_composition(crys_data["pretty_formula"]) 
-    cp.ValenceOrbital().featurize_dataframe(crys_data, col_id="composition")  
-
-    return y_new
+	return data
 
 def filter_data(df,elems,pname,pmin=None,pmax=None,stab=None):
+	'''Filter data by criteria'''
+
+	print('# entries before filters: ',len(df))
+
+	# filter by chemistry
+	inds = np.zeros((len(elems),len(df)))
+	for i,item in enumerate(elems):
+	  inds[i,:] = (df['pretty_formula'].str.contains(item))
+	  
+	idx = np.prod(inds,axis=0)
+	df = df[idx==1]
+	print('# entries after chemistry: ',len(df))
+
+	# filter by property values
+	if pmin:
+	  df = df[df[pname] >= pmin]
+	if pmax:
+	  df = df[df[pname] <= pmax]
+	print('# entries after property: ',len(df))
+	  
+	# filter by stability
+	if stab:
+	  df = df[df['e_above_hull'] <= stab]
+	print('# entries after stability: ',len(df))
+
+	return df
+
+def get_xy(df,elems,pname,pmin,pmax,stab):
+	'''Get x and y from data'''
+
+	# filter NaNs and entries based on criteria
+	df = df.dropna()
+	df = filter_data(df,elems,pname,pmin=pmin,pmax=pmax,stab=stab)
+
+	# exclude non-input columns
+	exclude = ['pretty_formula',pname,'e_above_hull','structure','composition','composition_oxid','radial distribution function']
+	
+	# get X and Y
+	x = df.sort_index().drop(exclude, axis=1)
+	y = df[pname].sort_index().values
+
+	return x,y
+
+def fit_forest(x,y,lbl='Full'):
+
+  # split data
+  x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+  # grid-search optimal parameters
+  rf = RandomForestRegressor()
+  param_grid = { 
+        'n_estimators'      : [10,25,50,100,250],
+        'max_features'      : ['auto','sqrt','log2'],
+        'min_samples_split' : [2,4,8],
+        'min_samples_leaf'  : [1, 2, 5]
+        }
+  grid = GridSearchCV(rf, param_grid, n_jobs=-1, cv=5)
+  grid.fit(x_train, y_train)
+
+  print(grid.best_score_)
+  print(grid.best_params_)
+  print(grid.score(x_test, y_test))
+
+  # use optimal parameters
+  rf.set_params(**grid.best_params_)
+  rf.fit(x_train, y_train)
+
+  y_hat_train = rf.predict(x_train) 
+  y_hat_test = rf.predict(x_test) 
+
+  mae_train = np.mean(abs(y_hat_train-y_train))/np.mean(y_train)
+  print('%s RF, train error: %.3f' % (lbl,mae_train))
+
+  mae_test = np.mean(abs(y_hat_test-y_test))/np.mean(y_test)
+  print('%s RF, test error : %.3f' % (lbl,mae_test))
+
+  return rf
+
+def fit_model(x,y,show_flag=False):
+
+  # fit RF using all variables
+  print('Fitting full random forest...')
+  rf = fit_forest(x,y,lbl='Full')
+
+  # variable importances
+  nvar = 10
+  imp = rf.feature_importances_
+  idx = np.argsort(imp)[::-1]
+  print('%d most important variables:' % nvar)
+  print(x.columns.values[idx][0:nvar])
+
+  # prune variables
+  thr = 0.5*np.median(imp)
+  idx = imp < thr
+  exclude = list(x.columns.values[idx])
+  x_sel = x.drop(exclude, axis=1)
+
+  # fit RF using important variables
+  print('\nFitting pruned random forest...')
+  rf = fit_forest(x_sel,y,lbl='Pruned')
   
-  # filter by chemistry
-  inds = np.zeros((len(elems),len(df)))
-  for i,item in enumerate(elems):
-    inds[i,:] = (df['pretty_formula'].str.contains(item))
-    
-  idx = np.prod(inds,axis=0)
-  df = df[idx==1]
-  print(len(df))
+  print('%d pruned variables:' % len(x_sel.columns))
+  print(x_sel.columns.values)
   
-  # filter by property values
-  if pmin:
-    df = df[df[pname] >= pmin]
-  if pmax:
-    df = df[df[pname] <= pmax]
-  print(len(df))
+  if show_flag:
+    # plt.figure(figsize=(7, 4))
+
+    # importance chart
+    plt.subplot(121)
     
-  # filter by stability
-  if stab:
-    df = df[df['e_above_hull'] <= stab]
+    ind = np.argsort(imp)[::-1]
+    plt.bar(x=x.columns.values[ind][0:nvar], height=imp[ind][0:nvar],color=(0.3,0.3,0.9))
+    plt.xticks(x.columns.values[ind][0:nvar], x.columns.values[ind][0:nvar], rotation='vertical')
+    plt.xlabel('Variables')
+    plt.ylabel('Importance')
+
+    # parity plot
+    ax = plt.subplot(122)
+    ax.set_aspect(1)
     
-  print(len(df))
-  
-  return df
+    plt.scatter(y, rf.predict(x_sel),marker='s',alpha=.25,c=(0.9,0.3,0.3))
+    plt.plot(np.arange(np.max(y)),c='gray')
+    plt.xlabel('Ground truth')
+    plt.ylabel('RF prediction')
+    
+    plt.subplots_adjust(bottom=0.25,top=0.75)
+    plt.draw()
+    plt.show()
+
+  return rf
 
 def add_atom_feats(df):
   
